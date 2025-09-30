d31e3c52a361c5eb4e14f712b38a04af2dd92c2e
Sami Gabol <samigabol12@gmail.com>
2025-09-30 03:02:58 +0000

    Try fixing this error: `Console Error: Error: Could not create payment i

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,11 @@
 import Stripe from 'stripe';
+import 'dotenv/config';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('STRIPE_SECRET_KEY is not set in the environment variables');
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2024-06-20',
   typescript: true,
 });
