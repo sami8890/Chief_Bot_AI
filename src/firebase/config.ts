e51efba994d7bf2b96afc2697dc593dbfd732e40
Sami Gabol <samigabol12@gmail.com>
2025-10-09 06:34:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1309829572-d62fc",
+  "appId": "1:263758338013:web:20722bc3ab9823a4df4834",
+  "apiKey": "AIzaSyCdQTzZgc4U_Dqtg4MgOdARrlO_xoR5qaU",
+  "authDomain": "studio-1309829572-d62fc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "263758338013"
+};
